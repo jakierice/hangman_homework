342902dcc8aee3f57f8f8149250a89531078ff38
Jakie Rice <jakie.rice@gmail.com>
2017-06-29 23:37:02 -0500

    working current guess block

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ window.onload = function () {
     document.onkeyup = function (event) {
 
         var key = event.keyCode;
-        var guess = event.key.toLowerCase();
+        var guess = event.key.toUpperCase();
 
-        // var guessedLetters = [];
         console.log(guess);
+        document.getElementById('guesses').style.display = 'block';
 
         for (var i = 0; i < 10; i++) {
-            document.getElementById('guessesLeft').innerHTML = "Guesses: " + guess;
+            document.getElementById('guesses').innerHTML = guess;
         }
     }
 }
