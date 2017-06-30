be9deaccdd8765b31ba24ac25e17d5015f01185a
Jakie Rice <jakie.rice@gmail.com>
2017-06-30 00:07:51 -0500

    working list of previous guesses

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 window.onload = function () {
+    var allGuesses = [];
 
     document.onkeyup = function (event) {
 
         var key = event.keyCode;
         var guess = event.key.toUpperCase();
 
-        console.log(guess);
-        document.getElementById('guesses').style.display = 'block';
+        document.getElementById('currentGuess').style.display = 'block';
+        document.getElementById('allGuesses').style.display = 'inline';
+        allGuesses.push(guess);
 
-        for (var i = 0; i < 10; i++) {
-            document.getElementById('guesses').innerHTML = guess;
+        for (var i = 0; i < allGuesses.length; i++) {
+            document.getElementById('currentGuess').innerHTML = guess;
         }
+
+        var guessedLetter = document.createElement('li');
+        guessedLetter.innerHTML = guess;
+        document.getElementById('guessList').appendChild(guessedLetter);
+
+        console.log(allGuesses);
     }
 }
